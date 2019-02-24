593f526b225963768c3153a35efce4ac55a08be4
hyfly233 <hyfly233@outlook.com>
2019-02-24 12:00:00 +0800

    commit

--- screenshot_demo/main.py
+++ screenshot_demo/main.py
@@ -1,6 +1,7 @@
 import os
 
 import fitz  # PyMuPDF
+import pymupdf.table
 from PIL import Image
 from dotenv import load_dotenv
 
@@ -9,21 +10,28 @@ load_dotenv()
 
 def main():
     pdf_path: str = os.getenv('PDF_PATH')
-    page_number: int = 0  # 第1页，0为起始
-    rect: tuple[int, int, int, int] = (100, 100, 400, 300)  # (x0, y0, x1, y1) 左上和右下坐标
+    pdf_basename = os.path.splitext(os.path.basename(pdf_path))[0]
 
     # 打开PDF
     doc: fitz.Document = fitz.open(pdf_path)
-    page: fitz.Page = doc[page_number]
 
-    # 截图区域
-    clip: fitz.Rect = fitz.Rect(rect)
-    pix: fitz.Pixmap = page.get_pixmap(clip=clip, dpi=200)  # 可调整dpi提高清晰度
+    if doc.page_count > 0:
+        for page in doc:
+            find_tables: pymupdf.table.TableFinder = fitz.find_tables(page)
+            for i, table in enumerate(find_tables.tables):
+                (x0, y0, x1, y1) = table.bbox
 
-    # 保存为图片
-    img: Image.Image = Image.frombytes("RGB", tuple[pix.width:int, pix.height:int], pix.samples)
-    img.save("screenshot.png")
-    print("截图已保存为 screenshot.png")
+                # # 截图区域
+                rect: tuple[int, int, int, int] = (x0, y0, x1, y1)  # (x0, y0, x1, y1) 左上和右下坐标
+                clip: fitz.Rect = fitz.Rect(rect)
+                pix: fitz.Pixmap = page.get_pixmap(clip=clip, dpi=200)  # 可调整dpi提高清晰度
+
+                # 保存为图片
+                img: Image.Image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+                png_name = f"screenshot_{pdf_basename}_page{page.number}_table{i}.png"
+                img.save(png_name)
+                print("截图已保存为:", png_name)
 
 
 if __name__ == '__main__':
