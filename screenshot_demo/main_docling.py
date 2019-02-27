95b5b43c78d2b4786184d396cdae2e2a61953821
hyfly233 <hyfly233@outlook.com>
2019-02-27 12:00:00 +0800

    commit

---
+++ screenshot_demo/main_docling.py
@@ -0,0 +1,17 @@
+import os
+
+import fitz  # PyMuPDF
+import pymupdf.table
+from PIL import (Image)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    pdf_path: str = os.getenv('PDF_PATH')
+    pdf_basename = os.path.splitext(os.path.basename(pdf_path))[0]
+
+
+if __name__ == '__main__':
+    main()
