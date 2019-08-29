89c315f69ba302f15ad03fedeca3259f419fe423
hyfly233 <hyfly233@outlook.com>
2019-08-29 12:00:00 +0800

    commit

--- word/mark_location.py
+++ word/mark_location.py
@@ -7,3 +7,8 @@ load_dotenv()
 def parse_document(file_path):
     if file_path.endswith('.docx'):
         pass
+
+
+if __name__ == '__main__':
+    word_path: str = os.getenv('WORD_PATH')
+    parse_document(word_path)
