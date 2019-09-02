13c7c75dbcd5d5b863b5049e97ada2cf290098a3
hyfly233 <hyfly233@outlook.com>
2019-09-02 12:00:00 +0800

    commit

---
+++ word/docx_demo/add_comment.py
@@ -0,0 +1,18 @@
+import os
+
+from docx import Document
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def add_comment(file_path):
+    if file_path.endswith('.docx'):
+        doc = Document(file_path)
+        doc.add_page_break()
+        paragraph = doc.add_paragraph("这是一个新的段落。")
+
+
+if __name__ == '__main__':
+    word_path: str = os.getenv('WORD_PATH')
+    add_comment(word_path)
--- word/docx_demo/parse_document_demo.py
+++ word/docx_demo/parse_document_demo.py
@@ -13,13 +13,10 @@ def parse_document(file_path):
             for run in para.runs:
                 text_blocks.append({
                     "text": run.text,
-                    "bbox": (0, 0, 0, 0),  # Word 文档中没有直接的 bbox 信息
-                    "page": 0,  # Word 文档没有页码概念
                     "font": run.font.name,
                     "size": run.font.size
                 })
         return text_blocks, doc
-
 
 
 if __name__ == '__main__':
@@ -27,4 +24,3 @@ if __name__ == '__main__':
     text_blocks, doc = parse_document(word_path)
     # print("text_blocks:", text_blocks)
     print("doc:", doc)
-
