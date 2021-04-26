1867def53fd4f3a9ac45d5813b5abfeda77c3f83
hyfly233 <hyfly233@outlook.com>
2021-04-26 12:00:00 +0800

    commit

--- word/docx_demo/add_comment.py
+++ word/docx_demo/add_comment.py
@@ -1,4 +1,6 @@
 import os
+import re
+from typing import List
 
 from docx import Document
 from dotenv import load_dotenv
@@ -6,7 +8,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def add_comments(file_path, word):
+def copy_font_format(source_font, target_font):
+    """复制字体格式"""
+    try:
+        if source_font.name:
+            target_font.name = source_font.name
+        if source_font.size:
+            target_font.size = source_font.size
+        if source_font.bold is not None:
+            target_font.bold = source_font.bold
+        if source_font.italic is not None:
+            target_font.italic = source_font.italic
+        if source_font.underline is not None:
+            target_font.underline = source_font.underline
+        if source_font.color.rgb:
+            target_font.color.rgb = source_font.color.rgb
+    except:
+        pass
+
+
+def add_comments(file_path: str, words: List[str]):
     if not file_path.endswith('.docx'):
         print("只支持 .docx 格式文件")
         return
@@ -16,50 +37,106 @@ def add_comments(file_path, word):
     base_name = os.path.splitext(file_path)[0]
     new_file_path = f"{base_name}_comments.docx"
 
+    if words is None or words is False or len(words) == 0:
+        print("没有提供任何词语进行添加注释")
+        return
+
+    # 按词语长度降序排序，优先处理长词，避免短词干扰长词的匹配
+    sorted_words = sorted(words, key=len, reverse=True)
+
     for i, paragraph in enumerate(doc.paragraphs):
-        p_text = paragraph.text
-        if word in p_text:
-            print(f"index: {i}, text: {p_text}")
+        full_text = paragraph.text
 
-            # 从后往前处理，避免索引变化问题
-            for j in range(len(paragraph.runs) - 1, -1, -1):
-                run = paragraph.runs[j]
-                run_text = run.text
-                if word in run_text:
-                    # 保存原run的格式
-                    original_font = run.font
+        # 检查是否包含任何目标词语
+        has_target_words = any(word in full_text for word in sorted_words)
+        if not has_target_words:
+            continue
 
-                    # 按目标词拆分文本
-                    parts = run_text.split(word)
+        print(f"index: {i}, original text: {full_text}")
 
-                    # 先清空当前run
-                    run.clear()
+        # 创建字符到格式的映射
+        char_formats = []
+        char_index = 0
 
-                    # 重新构建内容
-                    for k, part in enumerate(parts):
-                        if part:
-                            paragraph.add_run(part)
+        for run in paragraph.runs:
+            run_text = run.text
+            for char in run_text:
+                char_formats.append(run.font)
+                char_index += 1
 
-                        if k < len(parts) - 1:  # 不是最后一部分
-                            # 为目标词语创建单独的run
-                            target_run = paragraph.add_run(word)
-                            target_run.font.color.rgb = original_font.color.rgb
-                            target_run.font.name = original_font.name
-                            target_run.font.size = original_font.size
-                            target_run.font.bold = original_font.bold
+        # 构建正则表达式，匹配所有目标词语
+        pattern = '|'.join(re.escape(word) for word in sorted_words)
 
-                            # 添加注释
-                            doc.add_comment(
-                                runs=[target_run],
-                                text=f"这是对词语'{word}'的评论。",
-                                author="作者名(测试)",
-                                initials="作者(测试)"
-                            )
+        # 分割文本，保留分隔符
+        parts = re.split(f'({pattern})', full_text)
+
+        # 清空段落的所有runs
+        for run in paragraph.runs:
+            run.clear()
+
+        # 移除所有runs
+        while len(paragraph.runs) > 0:
+            paragraph._element.remove(paragraph.runs[0]._element)
+
+        # 重新构建段落内容
+        current_pos = 0
+        for part in parts:
+            if not part:  # 跳过空字符串
+                continue
+
+            if part in sorted_words:
+                # 添加目标词（需要添加注释的词）
+                target_run = paragraph.add_run(part)
+
+                # 继承原文字的格式
+                if current_pos < len(char_formats):
+                    source_font = char_formats[current_pos]
+                    copy_font_format(source_font, target_run.font)
+
+                # 为目标词添加注释
+                try:
+                    doc.add_comment(
+                        runs=[target_run],
+                        text=f"这是对词语'{part}'的评论。",
+                        author="作者名(测试)",
+                        initials="作者(测试)"
+                    )
+                except Exception as e:
+                    print(f"添加注释失败: {e}")
+                    # 如果添加注释失败，至少保持文本不变
+            else:
+                # 对于普通文本，按字符重建，保持原有格式
+                start_pos = current_pos
+                for char_idx, char in enumerate(part):
+                    format_idx = start_pos + char_idx
+                    if format_idx < len(char_formats):
+                        # 为每个字符创建单独的run（如果格式不同）
+                        if char_idx == 0 or (
+                                format_idx > 0 and char_formats[format_idx] != char_formats[format_idx - 1]):
+                            # 创建新的run
+                            char_run = paragraph.add_run(char)
+                            copy_font_format(char_formats[format_idx], char_run.font)
+                        else:
+                            # 添加到最后一个run
+                            if paragraph.runs:
+                                paragraph.runs[-1].text += char
+                    else:
+                        # 如果超出了格式范围，使用最后一个可用格式
+                        if char_idx == 0:
+                            char_run = paragraph.add_run(char)
+                            if char_formats:
+                                copy_font_format(char_formats[-1], char_run.font)
+                        else:
+                            if paragraph.runs:
+                                paragraph.runs[-1].text += char
+
+            current_pos += len(part)
+
+        print(f"index: {i}, processed text: {paragraph.text}")
 
     # 保存新文档
     doc.save(new_file_path)
     print(f"标注完成，新文件已保存为: {new_file_path}")
-
 
 
 if __name__ == '__main__':
