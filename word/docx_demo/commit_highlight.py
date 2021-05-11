4d73a0e7bf17aeac7c16f0ff27acff00d7cd12a4
hyfly233 <hyfly233@outlook.com>
2021-05-11 12:00:00 +0800

    commit

--- word/docx_demo/commit_highlight.py
+++ word/docx_demo/commit_highlight.py
@@ -1,9 +1,10 @@
 import os
 import re
 from dataclasses import dataclass
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Dict
 
 from docx import Document
+from docx.text.paragraph import Paragraph
 from dotenv import load_dotenv
 
 from word.docx_demo.common import copy_font_format
@@ -28,158 +29,220 @@ class AnnotationConfig:
     font_color: Optional[str] = None  # 字体颜色名称
 
 
-def annotate_words_with_configs(file_path: str, word_configs: dict[str, AnnotationConfig]):
+class DocumentAnnotator:
+    """文档标注器"""
+
+    def __init__(self, word_configs: Dict[str, AnnotationConfig]):
+        self.word_configs = word_configs
+        self.sorted_words = sorted(word_configs.keys(), key=len, reverse=True)
+        self.pattern = '|'.join(re.escape(word) for word in self.sorted_words)
+
+    def annotate_document(self, file_path: str) -> str:
+        """标注整个文档"""
+        if not file_path.endswith('.docx'):
+            raise ValueError("只支持 .docx 格式文件")
+
+        # 生成新文件名
+        base_name = os.path.splitext(file_path)[0]
+        new_file_path = f"{base_name}_标注版本.docx"
+
+        try:
+            doc = Document(file_path)
+
+            # 处理段落
+            print(f"段落数量: {len(doc.paragraphs)}")
+            self._process_paragraphs(doc.paragraphs, doc, "段落")
+
+            # 处理表格
+            print(f"表格数量: {len(doc.tables)}")
+            self._process_tables(doc.tables, doc)
+
+            # 保存文档
+            doc.save(new_file_path)
+            print(f"标注完成，新文件已保存为: {new_file_path}")
+            return new_file_path
+
+        except Exception as e:
+            print(f"处理文档时发生错误: {e}")
+            raise
+
+    def _process_tables(self, tables, doc):
+        """处理所有表格"""
+        for i, table in enumerate(tables):
+            for j, row in enumerate(table.rows):
+                for k, cell in enumerate(row.cells):
+                    if cell.text.strip():  # 只处理非空单元格
+                        print(f"表格 {i + 1}, 行 {j + 1}, 列 {k + 1}, 内容: {cell.text}")
+                        self._process_paragraphs(cell.paragraphs, doc, f"表格{i + 1}-行{j + 1}-列{k + 1}")
+
+    def _process_paragraphs(self, paragraphs: List[Paragraph], doc, context: str = ""):
+        """处理段落列表"""
+        for i, paragraph in enumerate(paragraphs):
+            self._process_single_paragraph(paragraph, doc, f"{context}-段落{i + 1}")
+
+    def _process_single_paragraph(self, paragraph: Paragraph, doc, context: str = ""):
+        """处理单个段落"""
+        full_text = paragraph.text
+
+        # 检查是否包含任何目标词语
+        if not any(word in full_text for word in self.sorted_words):
+            return
+
+        print(f"{context}, 内容: {full_text}")
+
+        try:
+            # 创建字符到格式的映射
+            char_formats = self._create_char_format_mapping(paragraph)
+
+            # 分割文本
+            parts = re.split(f'({self.pattern})', full_text)
+
+            # 清空并重建段落
+            self._clear_paragraph_runs(paragraph)
+            self._rebuild_paragraph(paragraph, parts, char_formats, doc)
+
+            print(f"  {context}, 处理后的内容: {paragraph.text}")
+
+        except Exception as e:
+            print(f"处理段落时发生错误 ({context}): {e}")
+
+    def _create_char_format_mapping(self, paragraph: Paragraph) -> List:
+        """创建字符到格式的映射"""
+        char_formats = []
+        for run in paragraph.runs:
+            for char in run.text:
+                char_formats.append(run.font)
+        return char_formats
+
+    def _clear_paragraph_runs(self, paragraph: Paragraph):
+        """清空段落的所有runs"""
+        for run in paragraph.runs:
+            run.clear()
+
+        while len(paragraph.runs) > 0:
+            paragraph._element.remove(paragraph.runs[0]._element)
+
+    def _rebuild_paragraph(self, paragraph: Paragraph, parts: List[str],
+                           char_formats: List, doc):
+        """重建段落内容"""
+        current_pos = 0
+
+        for part in parts:
+            if not part:  # 跳过空字符串
+                continue
+
+            if part in self.sorted_words:
+                self._add_annotated_word(paragraph, part, char_formats, current_pos, doc)
+            else:
+                self._add_normal_text(paragraph, part, char_formats, current_pos)
+
+            current_pos += len(part)
+
+    def _add_annotated_word(self, paragraph: Paragraph, word: str,
+                            char_formats: List, current_pos: int, doc):
+        """添加标注的词语"""
+        config = self.word_configs[word]
+
+        # 构建标注文本
+        annotated_word = word
+        if config.emphasize:
+            annotated_word = f"{config.emphasize_symbols[0]}{word}{config.emphasize_symbols[1]}"
+
+        # 添加run
+        target_run = paragraph.add_run(annotated_word)
+
+        # 继承原格式
+        if current_pos < len(char_formats):
+            copy_font_format(char_formats[current_pos], target_run.font)
+
+        # 应用标注格式
+        self._apply_annotation_format(target_run, config)
+
+        # 添加注释
+        if config.add_comment:
+            self._add_comment(doc, target_run, config, word)
+
+    def _add_normal_text(self, paragraph: Paragraph, text: str,
+                         char_formats: List, start_pos: int):
+        """添加普通文本，保持原有格式"""
+        for char_idx, char in enumerate(text):
+            format_idx = start_pos + char_idx
+
+            if format_idx < len(char_formats):
+                # 检查是否需要创建新run
+                need_new_run = (char_idx == 0 or
+                                (format_idx > 0 and
+                                 char_formats[format_idx] != char_formats[format_idx - 1]))
+
+                if need_new_run:
+                    char_run = paragraph.add_run(char)
+                    copy_font_format(char_formats[format_idx], char_run.font)
+                    # 保持原有颜色
+                    if char_formats[format_idx].color.rgb:
+                        char_run.font.color.rgb = char_formats[format_idx].color.rgb
+                else:
+                    # 添加到最后一个run
+                    if paragraph.runs:
+                        paragraph.runs[-1].text += char
+            else:
+                # 使用最后可用格式
+                self._add_char_with_fallback_format(paragraph, char, char_formats, char_idx)
+
+    def _add_char_with_fallback_format(self, paragraph: Paragraph, char: str,
+                                       char_formats: List, char_idx: int):
+        """使用备用格式添加字符"""
+        if char_idx == 0:
+            char_run = paragraph.add_run(char)
+            if char_formats:
+                copy_font_format(char_formats[-1], char_run.font)
+                if char_formats[-1].color.rgb:
+                    char_run.font.color.rgb = char_formats[-1].color.rgb
+        else:
+            if paragraph.runs:
+                paragraph.runs[-1].text += char
+
+    def _apply_annotation_format(self, run, config: AnnotationConfig):
+        """应用标注格式"""
+        if config.font_color:
+            _apply_font_color(run, config.font_color)
+
+        if config.highlight:
+            _apply_highlight_color(run, config.highlight_color)
+
+    def _add_comment(self, doc, run, config: AnnotationConfig, word: str):
+        """添加注释"""
+        try:
+            comment_text = config.comment_text or f"标注词语: {word}"
+            doc.add_comment(
+                runs=[run],
+                text=comment_text,
+                author=config.comment_author,
+                initials=config.comment_initials
+            )
+        except Exception as e:
+            print(f"添加注释失败: {e}")
+
+
+def annotate_words_with_configs(file_path: str, word_configs: Dict[str, AnnotationConfig]) -> str:
     """
     在文档中标注多个词语（为每个词语使用不同配置）
 
     Args:
         file_path: 源文档路径
         word_configs: 词语和配置的字典 {词语: 配置}
+
+    Returns:
+        新文件路径
     """
-    if not file_path.endswith('.docx'):
-        print("只支持 .docx 格式文件")
-        return
+    if not word_configs:
+        raise ValueError("没有提供任何词语进行标注")
 
-    # 生成新文件名
-    base_name = os.path.splitext(file_path)[0]
-    new_file_path = f"{base_name}_标注版本.docx"
-
-    doc = Document(file_path)
-    target_words = list(word_configs.keys())
-
-    if not target_words:
-        print("没有提供任何词语进行标注")
-        return
-
-    # 按词语长度降序排序，优先处理长词，避免短词干扰长词的匹配
-    sorted_words = sorted(target_words, key=len, reverse=True)
-
-    print(f"段落数量 {len(doc.paragraphs)}")
-
-    for i, paragraph in enumerate(doc.paragraphs):
-        full_text = paragraph.text
-
-        # 检查是否包含任何目标词语
-        has_target_words = any(word in full_text for word in sorted_words)
-        if not has_target_words:
-            continue
-
-        print(f"段落 {i + 1}, 内容: {full_text}")
-
-        # 创建字符到格式的映射
-        char_formats = []
-        char_index = 0
-
-        for run in paragraph.runs:
-            run_text = run.text
-            for char in run_text:
-                char_formats.append(run.font)
-                char_index += 1
-
-        # 构建正则表达式，匹配所有目标词语，如：喵喵公司|公司|喵
-        pattern = '|'.join(re.escape(word) for word in sorted_words)
-
-        # 分割文本，保留分隔符
-        parts = re.split(f'({pattern})', full_text)
-
-        # 清空段落的所有runs
-        for run in paragraph.runs:
-            run.clear()
-
-        # 移除所有runs
-        while len(paragraph.runs) > 0:
-            paragraph._element.remove(paragraph.runs[0]._element)
-
-        # 重新构建段落内容
-        current_pos = 0
-        for part in parts:
-            if not part:  # 跳过空字符串
-                continue
-
-            if part in sorted_words:
-                config = word_configs[part]
-
-                # 构建标注文本
-                annotated_word = part
-                if config.emphasize:
-                    annotated_word = f"{config.emphasize_symbols[0]}{part}{config.emphasize_symbols[1]}"
-
-                # 添加标注的目标词
-                target_run = paragraph.add_run(annotated_word)
-
-                # 继承原文字的格式（除了颜色）
-                if current_pos < len(char_formats):
-                    source_font = char_formats[current_pos]
-                    copy_font_format(source_font, target_run.font)
-
-                # 应用字体颜色
-                if config.font_color:
-                    _apply_font_color(target_run, config.font_color)
-
-                # 应用高亮
-                if config.highlight:
-                    _apply_highlight_color(target_run, config.highlight_color)
-
-                # 添加注释
-                if config.add_comment:
-                    try:
-                        comment_text = config.comment_text or f"标注词语: {part}"
-                        doc.add_comment(
-                            runs=[target_run],
-                            text=comment_text,
-                            author=config.comment_author,
-                            initials=config.comment_initials
-                        )
-                    except Exception as e:
-                        print(f"添加注释失败: {e}")
-            else:
-                # 对于普通文本，按字符重建，保持原有格式
-                start_pos = current_pos
-                for char_idx, char in enumerate(part):
-                    format_idx = start_pos + char_idx
-                    if format_idx < len(char_formats):
-                        # 为每个字符创建单独的run（如果格式不同）
-                        if char_idx == 0 or (
-                                format_idx > 0 and char_formats[format_idx] != char_formats[format_idx - 1]):
-                            # 创建新的run
-                            char_run = paragraph.add_run(char)
-                            copy_font_format(char_formats[format_idx], char_run.font)
-                            # 保持原有颜色
-                            if char_formats[format_idx].color.rgb:
-                                char_run.font.color.rgb = char_formats[format_idx].color.rgb
-                        else:
-                            # 添加到最后一个run
-                            if paragraph.runs:
-                                paragraph.runs[-1].text += char
-                    else:
-                        # 如果超出了格式范围，使用最后一个可用格式
-                        if char_idx == 0:
-                            char_run = paragraph.add_run(char)
-                            if char_formats:
-                                copy_font_format(char_formats[-1], char_run.font)
-                                if char_formats[-1].color.rgb:
-                                    char_run.font.color.rgb = char_formats[-1].color.rgb
-                        else:
-                            if paragraph.runs:
-                                paragraph.runs[-1].text += char
-
-            current_pos += len(part)
-
-        print(f"index: {i}, processed text: {paragraph.text}")
-
-    print("==============")
-
-    print(f"表格数量 {len(doc.tables)}")
-
-    for i, table in enumerate(doc.tables):
-        pass
-
-    # 保存新文档
-    doc.save(new_file_path)
-    print(f"标注完成，新文件已保存为: {new_file_path}")
+    annotator = DocumentAnnotator(word_configs)
+    return annotator.annotate_document(file_path)
 
 
-def annotate_multiple_words_same_config(file_path: str, target_words: List[str], config: AnnotationConfig):
+def annotate_multiple_words_same_config(file_path: str, target_words: List[str],
+                                        config: AnnotationConfig) -> str:
     """
     在文档中标注多个词语（使用相同配置）
 
@@ -187,10 +250,12 @@ def annotate_multiple_words_same_config(file_path: str, target_words: List[str],
         file_path: 源文档路径
         target_words: 要标注的词语列表
         config: 标注配置
+
+    Returns:
+        新文件路径
     """
-    # 创建相同配置的字典
     word_configs = {word: config for word in target_words}
-    annotate_words_with_configs(file_path, word_configs)
+    return annotate_words_with_configs(file_path, word_configs)
 
 
 def _apply_highlight_color(run, color_name: str):
@@ -231,7 +296,7 @@ def _apply_font_color(run, color_name: str):
     run.font.color.rgb = color_map.get(color_name.lower(), RGBColor(0, 0, 0))
 
 
-# 预设配置
+# 预设配置函数保持不变...
 def create_highlight_config(color: str = "yellow") -> AnnotationConfig:
     """创建高亮配置"""
     return AnnotationConfig(highlight=True, highlight_color=color)
@@ -272,14 +337,14 @@ if __name__ == '__main__':
     # 方案1: 多个词语使用不同配置
     word_configs = {
         "附件": create_highlight_config(color="red"),
-        "公司": create_highlight_config(color="yellow"),  # 黄色高亮
-        "喵": create_emphasize_config(symbols=("「", "」"), color="red"),  # 红色突出显示
+        "公司": create_highlight_config(color="yellow"),
+        "喵": create_emphasize_config(symbols=("「", "」"), color="red"),
         "卖方": create_full_annotation_config(
             comment_text="对卖方的评论",
             author="评论员1",
-            highlight_color="green",  # 绿色高亮
-            font_color="blue",  # 蓝色字体
-            symbols=("【", "】")  # 方括号
+            highlight_color="green",
+            font_color="blue",
+            symbols=("【", "】")
         ),
         "喵喵公司": create_full_annotation_config(
             comment_text="对喵喵公司的评论",
@@ -289,8 +354,17 @@ if __name__ == '__main__':
             symbols=("[", "]")
         ),
     }
-    annotate_words_with_configs(word_path, word_configs)
+
+    try:
+        new_file = annotate_words_with_configs(word_path, word_configs)
+        print(f"处理完成，生成文件: {new_file}")
+    except Exception as e:
+        print(f"处理失败: {e}")
 
     # 方案2: 多个词语使用相同配置
-    # same_config = create_emphasize_config(color="red")
-    # annotate_multiple_words_same_config(word_path, ["甲方", "乙方"], same_config)
+    # try:
+    #     same_config = create_emphasize_config(color="red")
+    #     new_file = annotate_multiple_words_same_config(word_path, ["甲方", "乙方"], same_config)
+    #     print(f"处理完成，生成文件: {new_file}")
+    # except Exception as e:
+    #     print(f"处理失败: {e}")
